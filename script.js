d72487c26b9548ea4e2fb61ea8694da59c3b1b59
td1b11 <77805940+td1b11@users.noreply.github.com>
2021-04-13 13:50:20 -0400

    Fixed Java

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
     $("#name").change(function(){
-        $("#Welcome to my website,").text("Welcome to my website, "+$("#name").val());
-        $("#tryit").click(function(){
-            $("#welcome").css("color","blue");
-        })
+        $("#welcome").text("Welcome to my website, "+$("#name").val());
+       
    
     });
+    $("#tryit").click(function(){
+        $("#welcome").css("color","blue");
+    })
 });
 
 
 $(document).ready(function(){
-    $("button").click(function(){
-        var food = $.get("https://foodish-api.herokuapp.com/");
+    $("#letseat").click(function(){
+        var food = $.get("https://foodish-api.herokuapp.com/api/");
 
         food.done(function(response){
-            $("#food").attr("src", response.message);
+            $("#food").attr("src", response.image);
         });
     })
 })
