71c5f2baba58eec5e47fc73040ff94b85f4ed1e9
td1b11 <77805940+td1b11@users.noreply.github.com>
2021-04-18 10:53:45 -0400

    Email

--- script.js
+++ script.js
@@ -20,4 +20,12 @@ $(document).ready(function(){
     })
 })
 
+$(document).ready(function(){
+    $("form").submit(function(){
+   event.preventDefault();
+   var webform = $.post("https://maker.ifttt.com/use/bbl3eu00UEI-S92NCIsxaX", json.stringify({"value1": $("#name").val, "value2": $("#email").val(), "value3": $("#message").val()}));
+
+    });
+})
+
 
